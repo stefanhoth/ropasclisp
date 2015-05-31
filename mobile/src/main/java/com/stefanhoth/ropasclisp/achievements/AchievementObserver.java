fb11a6a3b4e13b16c537217e7e698467054febcc
Stefan Hoth <stefan@novoda.com>
2015-06-01 01:53:38 +0200

    Correct behavior for daily start awards

--- mobile/src/main/java/com/stefanhoth/ropasclisp/achievements/AchievementObserver.java
+++ mobile/src/main/java/com/stefanhoth/ropasclisp/achievements/AchievementObserver.java
@@ -1,25 +1,36 @@
 package com.stefanhoth.ropasclisp.achievements;
 
+import android.content.Context;
+
 import com.stefanhoth.ropasclisp.games_sdk.PlayGamesClient;
 
 public class AchievementObserver {
 
     private final PlayGamesClient playGamesClient;
-    private final boolean firstSessionToday;
+    private final Context context;
 
-    public AchievementObserver(PlayGamesClient playGamesClient, boolean firstSessionToday) {
+    public AchievementObserver(PlayGamesClient playGamesClient, final Context context) {
         this.playGamesClient = playGamesClient;
-        this.firstSessionToday = firstSessionToday;
+        this.context = context;
     }
 
     public void onGameStart() {
         // first game start ever
         playGamesClient.unlockAchievement(ACHIEVEMENT.HELLO_THERE);
 
+        DAILY_STATE dailyState = DAILY_STATE.with(context);
+
         // on X game starts per day
-        if(! firstSessionToday){
-            playGamesClient.unlockAchievement(ACHIEVEMENT.CONNOISSEUR);
-            playGamesClient.unlockAchievement(ACHIEVEMENT.ADDICT);
+        if (!dailyState.isTodaysFirstStart()) {
+
+            final int todaysStarts = dailyState.getTodaysStarts();
+            if (ACHIEVEMENT.ADDICT.isGoalReached(todaysStarts)) {
+                playGamesClient.unlockAchievement(ACHIEVEMENT.ADDICT);
+            }
+            if (ACHIEVEMENT.CONNOISSEUR.isGoalReached(todaysStarts)) {
+                playGamesClient.unlockAchievement(ACHIEVEMENT.CONNOISSEUR);
+            }
+
         }
     }
 
