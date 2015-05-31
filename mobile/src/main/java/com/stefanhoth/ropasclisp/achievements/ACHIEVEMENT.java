af7aadae960c964daaf2bafb118a0316b00bad19
Stefan Hoth <stefan@novoda.com>
2015-06-01 01:52:28 +0200

    Update 2 achievement types, add goal amount to type + accessor method

--- mobile/src/main/java/com/stefanhoth/ropasclisp/achievements/ACHIEVEMENT.java
+++ mobile/src/main/java/com/stefanhoth/ropasclisp/achievements/ACHIEVEMENT.java
@@ -7,23 +7,30 @@ import com.stefanhoth.ropasclisp.R;
 
 public enum ACHIEVEMENT {
 
-    HELLO_THERE(R.string.achievement_hellothere, TYPE.STATIC),
-    GOTCHA(R.string.achievement_gotcha, TYPE.STATIC),
+    HELLO_THERE(R.string.achievement_hellothere, TYPE.STATIC, 1),
+    GOTCHA(R.string.achievement_gotcha, TYPE.STATIC, 1),
     ON_A_ROLL(R.string.achievement_onaroll, TYPE.INCREMENTAL),
     POKER_FACE(R.string.achievement_pokerface, TYPE.INCREMENTAL),
-    CONNOISSEUR(R.string.achievement_connoisseur, TYPE.INCREMENTAL),
-    ADDICT(R.string.achievement_addict, TYPE.INCREMENTAL);
+    CONNOISSEUR(R.string.achievement_connoisseur, TYPE.STATIC, 5),
+    ADDICT(R.string.achievement_addict, TYPE.STATIC, 25);
 
+    public static final int NO_GOAL_AMOUNT = 0;
     private final int achievementId;
     private final TYPE achievementType;
+    private final int goalAmount;
 
     private enum TYPE{
         STATIC, INCREMENTAL;
     }
 
     ACHIEVEMENT(@StringRes int achievementId, TYPE achievementType) {
+        this(achievementId, achievementType, NO_GOAL_AMOUNT);
+    }
+
+    ACHIEVEMENT(@StringRes int achievementId, TYPE achievementType, int goalAmount) {
         this.achievementId = achievementId;
         this.achievementType = achievementType;
+        this.goalAmount = goalAmount;
     }
 
     public String getAchievementId(final Context context) {
@@ -32,5 +39,9 @@ public enum ACHIEVEMENT {
 
     public boolean isIncremental() {
         return achievementType == TYPE.INCREMENTAL;
+    }
+
+    public boolean isGoalReached(final int actual) {
+        return actual >= goalAmount;
     }
 }
